61d215c66ebc6bdc98a94e2fc7c40246bfcbd820
Mei Rose Connor <meirconnor@gmail.com>
2025-04-01 10:26:17 -0400

    finished sorting functions

--- sorting.js
+++ sorting.js
@@ -4,10 +4,18 @@ function getProductTitles() {
   let productTitles = {};
   for (let product of products) {
     let productTitle = product.parentElement;
-    console.log(productTitle.dataset.title)
     productTitles[productTitle.dataset.title] = productTitle
 }
 return productTitles
+}
+
+function getProductPrices(){
+    let productPrices = {};
+    for (let product of products){
+        let productPrice = product.parentElement;
+        productPrices[productPrice.dataset.price] = productPrice
+    }
+    return productPrices
 }
 
 const form = document.querySelector("form");
@@ -16,16 +24,79 @@ const log = document.querySelector("#log");
 form.addEventListener("change", (event) => {
   if (event.target.type === "radio") {
     const selectedValue = event.target.value;
-    console.log(selectedValue);
+    if(selectedValue == "AZ"){
+        sortAZ()
+    }
+    else if (selectedValue == "ZA"){
+        sortZA()
+    }
+    else if (selectedValue == "Hi-Lo"){
+        sortHiLo()
+    }
+    else if (selectedValue== "Lo-Hi"){
+        sortLoHi()
+    }
   }
 });
 
 function sortAZ(){
     let myProductTitles = []
+    let myProductDivIds= []
     for (let productTitle in getProductTitles()){
         myProductTitles.push(productTitle)
     }
-    return myProductTitles.sort()
+    myProductTitles.sort()
+    for(let product of myProductTitles){
+        myProductDivIds.push(getProductTitles()[product].getAttribute("id"))
+    }
+    for (let id of myProductDivIds){
+    document.getElementById("all-items").appendChild(document.getElementById(id))
+    }
 }
 
+function sortZA(){
+    let myProductTitles = []
+    let myProductDivIds= []
+    for (let productTitle in getProductTitles()){
+        myProductTitles.push(productTitle)
+    }
+    myProductTitles.sort()
+    myProductTitles.reverse()
+    for(let product of myProductTitles){
+        myProductDivIds.push(getProductTitles()[product].getAttribute("id"))
+    }
+    for (let id of myProductDivIds){
+    document.getElementById("all-items").appendChild(document.getElementById(id))
+    }
+}
 
+function sortLoHi(){
+    let myProductPrices = []
+    let myProductDivIds = []
+    for (let productPrice in getProductPrices()){
+        myProductPrices.push(productPrice)
+    }
+    myProductPrices.sort((a,b)=>a-b)
+    for(let product of myProductPrices){
+        myProductDivIds.push(getProductPrices()[product].getAttribute("id"))
+    }
+    for (let id of myProductDivIds){
+    document.getElementById("all-items").appendChild(document.getElementById(id))
+    }
+}
+
+function sortHiLo(){
+    let myProductPrices = []
+    let myProductDivIds = []
+    for (let productPrice in getProductPrices()){
+        myProductPrices.push(productPrice)
+    }
+    myProductPrices.sort((a,b)=>a-b)
+    myProductPrices.reverse()
+    for(let product of myProductPrices){
+        myProductDivIds.push(getProductPrices()[product].getAttribute("id"))
+    }
+    for (let id of myProductDivIds){
+    document.getElementById("all-items").appendChild(document.getElementById(id))
+    }
+}
